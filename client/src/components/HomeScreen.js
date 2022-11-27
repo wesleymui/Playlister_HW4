9c6a07f2d5b5abedbd531264906b5628224d2334
Wesley Mui <wesley.mui.1@stonybrook.edu>
2022-11-27 00:23:41 -0500

    Added overflow for lists and player controller

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -7,7 +7,7 @@ import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab'
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography'
-import { AppBar, Box, Divider, Grid, IconButton, Menu, MenuItem, Tab, Tabs, TextField, Toolbar } from '@mui/material';
+import { AppBar, Box, Card, CardContent, Container, Divider, Grid, Icon, IconButton, Menu, MenuItem, Tab, Tabs, TextField, Toolbar } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import GroupIcon from '@mui/icons-material/Group';
 import PersonIcon from '@mui/icons-material/Person';
@@ -16,6 +16,11 @@ import Home from '@mui/icons-material/Home';
 import { borderRadius } from '@mui/system';
 import YoutubePlayer from './YoutubePlayer';
 import YouTube from 'react-youtube';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import { FixedSizeList } from 'react-window';
 
 /*
     This React component lists all the playlists in the UI.
@@ -59,7 +64,7 @@ const HomeScreen = () => {
     let listCard = "";
     if (store) {
         listCard = 
-            <List sx={{ width: '90%', left: '5%', bgcolor: '#123456', borderRadius: 1}}>
+            <List sx={{ maxWidth: '90%', left: '5%', bgcolor: '#123456', borderRadius: 1, maxHeight: 600, overflow: 'auto'}}>
             {
                 store.idNamePairs.map((pair) => (
                     <div>
@@ -97,25 +102,48 @@ const HomeScreen = () => {
                                 sx={{bgcolor: 'white', borderRadius: 1, width: 500}}/>
                         </Box>
                         <Box>
-                        <IconButton>
-                            <SortIcon fontSize='large' sx={{color: 'white'}} onClick={handleSortMenuOpen}></SortIcon>
-                        </IconButton>
+                            <IconButton>
+                                <SortIcon fontSize='large' sx={{color: 'white'}} onClick={handleSortMenuOpen}></SortIcon>
+                            </IconButton>
                         </Box>
                     </Toolbar>
                 </AppBar>
             </Grid>
             <Grid item xs={6}>
-                <Box sx={{backgroundColor: '#4e76cb'}}>
+                <Box sx={{backgroundColor: '#4e76cb', paddingTop: 2, paddingBottom: 2}}>
                     { listCard }
                 </Box>
             </Grid>
             <Grid item xs={6}>
                 <Box sx={{backgroundColor: '#4e76cb'}}>
-                    <Tabs textColor='white'>
-                        <Tab label="Player"/>
-                        <Tab label="Comments"/>
+                    <Tabs indicatorColor='black' textColor='white'>
+                        <Tab sx={{color: 'white'}} label="Player"/>
+                        <Tab sx={{color: 'white'}} label="Comments"/>
                     </Tabs>
-                    <YouTube videoId='xgiN9zNZCuM'></YouTube>
+                    <Box sx={{ml: 6, mt: 2}}><YouTube videoId='xgiN9zNZCuM'></YouTube></Box>
+                    <Box sx={{bgColor: 'blue'}}>
+                        <Card>
+                            <CardContent>
+                                <Typography component="div" variant="h6" sx={{fontWeight: 700}}>Now Playing</Typography>
+                                <Typography component="div" variant='p'>Playlist:</Typography>
+                                <Typography component="div" variant='p'>Song #:</Typography>
+                                <Typography component="div" variant='p'>Title:</Typography>
+                                <Typography component="div" variant='p'>Artist:</Typography>
+                                <IconButton>
+                                    <SkipPreviousIcon fontSize='large'></SkipPreviousIcon>
+                                </IconButton>
+                                <IconButton>
+                                    <PauseIcon fontSize='large'></PauseIcon>
+                                </IconButton>
+                                <IconButton>
+                                    <PlayArrowIcon fontSize='large'></PlayArrowIcon>
+                                </IconButton>
+                                <IconButton>
+                                    <SkipNextIcon fontSize='large'></SkipNextIcon>
+                                </IconButton>
+                            </CardContent>
+                        </Card>
+                    </Box>
                 </Box>
             </Grid>
             <Grid item xs={12}>
