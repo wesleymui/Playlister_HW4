18be71aca0a5eb47373c84f8e2248c7b934501c2
Wesley Mui <wesley.mui.1@stonybrook.edu>
2022-12-04 15:55:58 -0500

    Set text theme for home screen

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -7,7 +7,7 @@ import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab'
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography'
-import { AppBar, Box, Card, CardContent, Container, Divider, Grid, Icon, IconButton, Menu, MenuItem, Tab, Tabs, TextField, Toolbar } from '@mui/material';
+import { AppBar, Box, Card, CardContent, Container, createTheme, Divider, Grid, Icon, IconButton, Menu, MenuItem, Tab, Tabs, TextField, ThemeProvider, Toolbar } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import GroupIcon from '@mui/icons-material/Group';
 import PersonIcon from '@mui/icons-material/Person';
@@ -39,6 +39,23 @@ const HomeScreen = () => {
         store.createNewList();
     }
 
+    const theme = createTheme ({
+        typography: {
+          fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+          ].join(','),
+        },
+    });
+
     let listCard = "";
     if (store) {
         listCard = 
@@ -59,59 +76,62 @@ const HomeScreen = () => {
     }
     return (
         <Grid container sx={{backgroundColor: '#4e76cb', pb: 2}}>
-            <Grid item xs={12} sx={{flexGrow: 1}}>
-                <NavBar/>
-            </Grid>
-            <Grid item xs={6}>
-                <Box sx={{backgroundColor: '#4e76cb', pt: 2}}>
-                    { listCard }
-                </Box>
-            </Grid>
-            <Grid item xs={6}>
-                <Box sx={{backgroundColor: '#4e76cb'}}>
-                    <Tabs indicatorColor='white' textColor='white'>
-                        <Tab sx={{color: 'white', borderColor: 'black'}} label="Player"/>
-                        <Tab sx={{color: 'white'}} label="Comments"/>
-                    </Tabs>
-                    <Box sx={{ml: 6, mt: 2}}><YouTube videoId='xgiN9zNZCuM'></YouTube></Box>
-                    <Box sx={{bgColor: 'blue'}}>
-                        <Card>
-                            <CardContent>
-                                <Typography component="div" variant="h6" sx={{fontWeight: 700}}>Now Playing</Typography>
-                                <Typography component="div" variant='p'>Playlist:</Typography>
-                                <Typography component="div" variant='p'>Song #:</Typography>
-                                <Typography component="div" variant='p'>Title:</Typography>
-                                <Typography component="div" variant='p'>Artist:</Typography>
-                                <IconButton>
-                                    <SkipPreviousIcon fontSize='large'></SkipPreviousIcon>
-                                </IconButton>
-                                <IconButton>
-                                    <PauseIcon fontSize='large'></PauseIcon>
-                                </IconButton>
-                                <IconButton>
-                                    <PlayArrowIcon fontSize='large'></PlayArrowIcon>
-                                </IconButton>
-                                <IconButton>
-                                    <SkipNextIcon fontSize='large'></SkipNextIcon>
-                                </IconButton>
-                            </CardContent>
-                        </Card>
+            <ThemeProvider theme={theme}>
+                <Grid item xs={12} sx={{flexGrow: 1}}>
+                    <NavBar/>
+                </Grid>
+                <Grid item xs={6}>
+                    <Box sx={{backgroundColor: '#4e76cb', pt: 2}}>
+                        { listCard }
                     </Box>
-                </Box>
-            </Grid>
-            <Grid item xs={12}>
-                <Box display="flex" justifyContent="center" alignItems="center" sx={{width: '100%', pt: 1, backgroundColor: '#4e76cb'}}>
-                        <Fab 
-                            color="primary" 
-                            aria-label="add"
-                            id="add-list-button"
-                            onClick={handleCreateNewList}
-                        >
-                            <AddIcon />
-                        </Fab>
-                            <Typography variant="h4" color="white">Your Lists</Typography>
-                </Box>
-            </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                    <Box sx={{backgroundColor: '#4e76cb'}}>
+                        <Tabs indicatorColor='white' textColor='white'>
+                            <Tab sx={{color: 'white', borderColor: 'black'}} label="Player"/>
+                            <Tab sx={{color: 'white'}} label="Comments"/>
+                        </Tabs>
+                        <Box sx={{ml: 6, mt: 2}}><YouTube videoId='xgiN9zNZCuM'></YouTube></Box>
+                        <Box sx={{bgColor: 'blue'}}>
+                            <Card>
+                                <CardContent>
+                                    <Typography component="div" variant="h6" sx={{fontWeight: 700}}>Now Playing</Typography>
+                                    <Typography component="div" variant='p'>Playlist:</Typography>
+                                    <Typography component="div" variant='p'>Song #:</Typography>
+                                    <Typography component="div" variant='p'>Title:</Typography>
+                                    <Typography component="div" variant='p'>Artist:</Typography>
+                                    <IconButton>
+                                        <SkipPreviousIcon fontSize='large'></SkipPreviousIcon>
+                                    </IconButton>
+                                    <IconButton>
+                                        <PauseIcon fontSize='large'></PauseIcon>
+                                    </IconButton>
+                                    <IconButton>
+                                        <PlayArrowIcon fontSize='large'></PlayArrowIcon>
+                                    </IconButton>
+                                    <IconButton>
+                                        <SkipNextIcon fontSize='large'></SkipNextIcon>
+                                    </IconButton>
+                                </CardContent>
+                            </Card>
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item xs={12}>
+                    <Box display="flex" justifyContent="center" alignItems="center" sx={{width: '100%', pt: 1, backgroundColor: '#4e76cb'}}>
+                            <Fab 
+                                color="primary" 
+                                aria-label="add"
+                                id="add-list-button"
+                                onClick={handleCreateNewList}
+                            >
+                                <AddIcon />
+                            </Fab>
+                                <Typography variant="h4" color="white">Your Lists</Typography>
+                    </Box>
+                </Grid>
+                <MUIDeleteModal/>
+            </ThemeProvider>
         </Grid>
         /*<div id="playlist-selector">
             <div id="list-selector-heading">
